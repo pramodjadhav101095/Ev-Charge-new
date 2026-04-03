0177069e4d5b8599773c02e971e4eeb6f1b84169
Pramod Jadhav <pramodjadhav101095@gmail.com>
2026-04-03 12:14:13 +0530

    test: add junit unit tests for WhatsAppSender

---
+++ backend/notification-service/src/test/java/com/evcharge/notification/service/sender/WhatsAppSenderTest.java
@@ -0,0 +1,67 @@
+package com.evcharge.notification.service.sender;
+
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.rest.api.v2010.account.MessageCreator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockedStatic;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class WhatsAppSenderTest {
+
+    @InjectMocks
+    private WhatsAppSender whatsAppSender;
+
+    @BeforeEach
+    void setUp() {
+        // Set values for @Value fields using ReflectionTestUtils
+        ReflectionTestUtils.setField(whatsAppSender, "accountSid", "test_sid");
+        ReflectionTestUtils.setField(whatsAppSender, "authToken", "test_token");
+        ReflectionTestUtils.setField(whatsAppSender, "twilioNumber", "test_number");
+    }
+
+    @Test
+    void testSendWhatsApp_Success() {
+        // We use MockedStatic to mock the static Message.creator() method of Twilio SDK
+        try (MockedStatic<Message> mockedMessage = mockStatic(Message.class)) {
+            MessageCreator creator = mock(MessageCreator.class);
+            Message mockMessageResult = mock(Message.class);
+
+            // Mock the fluent API chain: Message.creator(...).create()
+            mockedMessage.when(() -> Message.creator(
+                    any(com.twilio.type.PhoneNumber.class),
+                    any(com.twilio.type.PhoneNumber.class),
+                    anyString())).thenReturn(creator);
+            
+            when(creator.create()).thenReturn(mockMessageResult);
+            when(mockMessageResult.getSid()).thenReturn("SM12345");
+
+            // Execute the send
+            whatsAppSender.send("+911234567890", "Booking Confirmed", "Your slot is ready!");
+
+            // Verify that Twilio's Message.creator() and create() were called once
+            mockedMessage.verify(() -> Message.creator(any(), any(), anyString()), times(1));
+            verify(creator, times(1)).create();
+        }
+    }
+
+    @Test
+    void testSendWhatsApp_MockMode() {
+        // Clear SID to trigger Mock Mode
+        ReflectionTestUtils.setField(whatsAppSender, "accountSid", "");
+
+        // Execute the send
+        whatsAppSender.send("+911234567890", "Booking Confirmed", "Your slot is ready!");
+
+        // If in Mock mode, nothing should be called from Twilio
+        // No Exception should be thrown either
+    }
+}
